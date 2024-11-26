eceaee6bcc8dcdf475baf275a1b03be756f808e6
Michel Lao <SansNom01@proton.me>
2024-11-26 13:02:01 +0100

    Check if files exists before

--- sway.zsh
+++ sway.zsh
@@ -1,3 +1,3 @@
-if [[ "$(cat /etc/hostname)" == "nanashi-hvywxx9" ]] && [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
+if [ -f /etc/hostname ] && [[ "$(cat /etc/hostname)" == "nanashi-hvywxx9" ]] && [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
 	exec sway > /tmp/sway.log
 fi
