164e3f83fe96191a8ef81cde7d4ab3133f068519
Michel Lao <SansNom01@proton.me>
2025-09-25 16:36:05 +0200

    Add composer vendor bin PATH

--- local-bin.zsh
+++ local-bin.zsh
@@ -1,7 +1,10 @@
 typeset -U path PATH
 tmp=()
 if (( $+commands[brew] )); then
-    tmp+="$(brew --prefix rustup)/bin"
+  tmp+="$(brew --prefix rustup)/bin"
+fi
+if (( $+commands[composer] )); then
+  tmp+="$(composer global -q config bin-dir --absolute)"
 fi
 path=(~/.local/bin ~/.cargo/bin $tmp $path)
 export PATH
